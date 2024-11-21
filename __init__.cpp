372dc79e8b0664de60d5d27ed82aab1034c478b5
Vandal <bjpowerll12967@gmail.com>
2024-11-21 20:18:25 +0900

    오후 8:18 2024-11-21

--- __init__.cpp
+++ __init__.cpp
@@ -13,7 +13,8 @@ void Solution()
 
 int main(int argc, char const *argv[])
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     Solution();
-
     return 0;
 }
--- 수_찾기.cpp
+++ 수_찾기.cpp
@@ -6,11 +6,19 @@
 
 using namespace std;
 
-bool bs(vector<int> &v, int n)
+bool bs(vector<int> &v, int target)
 {
-    int i = v.size() - 1;
+    int left = 0;
+    int right = v.size() - 1;
 
-    while (true)
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+
+        if (v[mid] == target) return true;
+        else if (v[mid] > target) right = mid - 1;
+        else if (v[mid] < target) left = mid + 1;
+    }
 
     return false;
 }
@@ -18,19 +26,27 @@ bool bs(vector<int> &v, int n)
 void Solution()
 {
     int n, m;
+
     cin >> n;
     vector<int> v(n);
     for (int i = 0; i < n; i++)
-        cin >> v[i];
+        cin >> v[i];    
 
     sort(v.begin(), v.end());
 
     cin >> m;
+    for (int i = 0; i < m; i++)
+    {
+        int t;
+        cin >> t;
+        cout << bs(v, t) << '\n';
+    }   
 }
 
 int main(int argc, char const *argv[])
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     Solution();
-
     return 0;
 }
