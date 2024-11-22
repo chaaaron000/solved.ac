ca07e447290b6982b26e567d7adc592f284d88d1
Vandal <bjpowerll12967@gmail.com>
2024-11-22 15:18:46 +0900

    오후 3:18 2024-11-22

--- 설탕_배달.cpp
+++ 설탕_배달.cpp
@@ -14,7 +14,21 @@ using namespace std;
 
 void Solution()
 {
+    int n;
+    cin >> n;
 
+    int min = -1;
+    for (int b5 = 0; b5 <= (n / 5); b5++)
+    {
+        int remain = n - (b5 * 5);
+        if (remain % 3 == 0)
+        {
+            int b3 = remain / 3;
+            min = (min == -1 || b3 + b5 < min ? b3 + b5 : min);
+        }
+    }
+
+    cout << min;
 }
 
 int main(int argc, char const *argv[])
--- 카드_2.cpp
+++ 카드_2.cpp
@@ -14,7 +14,25 @@ using namespace std;
 
 void Solution()
 {
+    int n;
+    cin >> n;
+    
+    queue<int> q;
+    for (int i = 0; i < n; i++)
+        q.push(i + 1);
 
+    while (q.size() > 1)
+    {
+        q.pop();
+
+        if (q.size() == 1) break;
+
+        int front = q.front();
+        q.pop();
+        q.push(front);
+    }
+
+    cout << q.front();
 }
 
 int main(int argc, char const *argv[])
